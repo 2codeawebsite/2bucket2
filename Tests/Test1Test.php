b04c50e08a63037de9f8b95dcc8097e12cc1b920
unknown <David@.(none)>
2013-08-29 20:15:01 +0200

    change test to pass

--- Tests/Test1Test.php
+++ Tests/Test1Test.php
@@ -7,7 +7,7 @@ class TestTest1 extends PHPUnit_Framework_TestCase {
 	public function testCheckNum() {
         $check = new TestClass1();
 
-        $this->assertFalse($check->checkNum(3, 2));
+        $this->assertFalse($check->checkNum(1, 2));
     }
 
 }
