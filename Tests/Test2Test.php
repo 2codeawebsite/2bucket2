98d7e6759ad5691884839ee3a55d013dac338a53
unknown <David@.(none)>
2013-08-29 20:33:46 +0200

    delete test1

--- Tests/Test2Test.php
+++ Tests/Test2Test.php
@@ -1,11 +1,10 @@
 <?php
 
-class TestTest2 extends PHPUnit_Framework_TestCase {
+class Test2Test extends PHPUnit_Framework_TestCase {
 	
 	
 	public function testCheckNum2() {
         $check = new Test2();
-
         $this->assertFalse($check->checkNum(1, 2));
     }
 
